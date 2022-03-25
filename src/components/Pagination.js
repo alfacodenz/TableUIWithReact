92e1fe3ff9a023bee5b3afa341a6dd5447c764df
sajberpapak <sajberpapak@yandex.com>
2022-03-25 08:44:12 +0100

    Added Pagination size functionality.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const Pagination = ({
   totalEmployees,
   employeesPerPage,
@@ -8,6 +10,17 @@ export const Pagination = ({
   for (let i = 1; i <= Math.ceil(totalEmployees / employeesPerPage); i++) {
     numbers.push(i);
   }
+
+  const [isBig, setIsBig] = useState(false);
+
+  const handleButtonBig = () => {
+    setIsBig(true);
+  };
+
+  const handleButtonSmall = () => {
+    setIsBig(false);
+  };
+
   return (
     <nav>
       <ul className="pagination">
@@ -15,13 +28,24 @@ export const Pagination = ({
           <li className="page-item" key={number}>
             <a
               href="#"
-              className="page-link"
+              className={`page-link ${isBig ? "big" : "small"} `}
               onClick={() => changePage(number)}>
               {number}
             </a>
           </li>
         ))}
       </ul>
+      <div className="container my-5">
+        <span>Choose Pagination size</span>
+        <button
+          className="btn btn-primary small mx-2"
+          onClick={handleButtonSmall}>
+          Small
+        </button>
+        <button className="btn btn-primary" onClick={handleButtonBig}>
+          Big
+        </button>
+      </div>
     </nav>
   );
 };
